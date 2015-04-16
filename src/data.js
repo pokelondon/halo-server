80cad30033e4225e5c557abd706223f68ac044c9
James Tiplady <biggleszx@biggleszx.com>
2015-04-16 12:24:59 +0100

    New endpoint defs

--- src/data.js
+++ src/data.js
@@ -16,6 +16,30 @@ var WEBHOOKS = {
         title: 'Graphic Equaliser',
         description: 'Lights move in time with the music on PokeRadio',
     },
+    'track-yay': {
+        slug: 'track-yay',
+        title: 'Track YAY',
+        description: 'Display a positive reaction to a POKE Radio track',
+        params: {title: 'The Sound of Silence', artist: 'Simon and Garfunkel'}
+    },
+    'track-noo': {
+        slug: 'track-noo',
+        title: 'Track NOO',
+        description: 'Display a negative reaction to a POKE Radio track',
+        params: {title: 'The Sound of Silence', artist: 'Simon and Garfunkel'}
+    },
+    'track-scratch': {
+        slug: 'track-scratch',
+        title: 'Track Scratch',
+        description: 'Display a reaction to a POKE Radio track being binned off',
+        params: {title: 'The Sound of Silence', artist: 'Simon and Garfunkel'}
+    },
+    'track-change': {
+        slug: 'track-change',
+        title: 'Track Change',
+        description: 'Show details for the track POKE Radio is now playing',
+        params: {title: 'The Sound of Silence', artist: 'Simon and Garfunkel'}
+    },
 };
 
 module.exports.WEBHOOKS = WEBHOOKS;
