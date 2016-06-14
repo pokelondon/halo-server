bddb309b67a9145b14cfaaa39f52e8edb4ec080d
Jamie Ingram <jamie@pokelondon.com>
2016-06-14 07:58:32 +0100

    brain half asleep

--- src/data.js
+++ src/data.js
@@ -93,7 +93,7 @@ var WEBHOOKS = {
     'goal': {
         slug: 'goal',
         title: 'Euro 2016 - the rich ball kickers kicked the ball in the net',
-        description: 'Ball kicking'
+        description: 'Ball kicking',
         params: {score: '0-0', team_1: 'ENG', team_2: 'ESP'}
     }
 };
